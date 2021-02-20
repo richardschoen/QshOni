55d001df83ea6cf8c9e24420bc02d36dfdd57190
Richard Schoen <richard@richardschoen.net>
2021-02-20 10:42:29 -0600

    Update and rename getrepo.sh to getrepoqshoni.sh

---
+++ getrepoqshoni.sh
@@ -0,0 +1,23 @@
+#!/QOpenSys/usr/bin/qsh
+#----------------------------------------------------------------
+# Script name: getrepo.sh
+# Author: Richard Schoen
+# Purpose: Get QSHONI repo and build the QSHONI library.
+# **WARNING** - Make sure to back up existing QSHONI library instances 
+#               before running build.sh
+#               Existing QSHONI library will be cleared by build.sh
+#----------------------------------------------------------------
+INSTALLTEMP="/tmp"
+INSTALLQSHONI="/tmp/QshOni"
+REPONAME="https://github.com/richardschoen/QshOni.git"
+
+# Create temp download IFS location
+mkdir ${INSTALLTEMP}
+cd /${INSTALLTEMP}
+# Clone the repo via git to temporary download location
+git -c http.sslVerify=false clone --recurse-submodules ${REPONAME}
+
+# Change to IFS temp download directory for repo and call build.sh to create library
+cd ${INSTALLQSHONI}
+bash build.sh
+# After installation you can manually delete the INSTALLQSHONI IFS directory /tmp/QshOni
