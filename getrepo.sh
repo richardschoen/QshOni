b06fd7c0ef58188f5039b3592040ae4c13d03479
Richard Schoen <richard@richardschoen.net>
2021-02-20 10:32:39 -0600

    Update getrepo.sh

--- getrepo.sh
+++ getrepo.sh
@@ -1,1 +1,21 @@
+#!/QOpenSys/usr/bin/qsh
+#----------------------------------------------------------------
+# Script name: getrepo.sh
+# Author: Richard Schoen
+# Purpose: Get QSHONI repo
+#----------------------------------------------------------------
+INSTALLTEMP="/tmp/qshoniinst"
+REPONAME="https://github.com/richardschoen/QshOni.git"
+SRCLIB="QSHONI"
+SRCLIBTEXT="QShell on IBM i"
+SRCFILE="SOURCE"
+installpath="/tmp/qshoniinst"
 
+# Create temp download location
+mkdir ${INSTALLTEMP}
+cd /${INSTALLTEMP}
+# Clone the repo via git
+git -c http.sslVerify=false clone --recurse-submodules ${REPONAME}
+# Change to download directory and call build.sh to create library
+cd ${INSTALLTEMP}
+#build.sh
