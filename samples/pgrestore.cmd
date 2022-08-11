3467926bb4330a03bbdda3723c05cf4514647397
Richard Schoen <richard@richardschoen.net>
2022-08-11 11:48:58 -0500

    Create pgrestore.cmd

---
+++ samples/pgrestore.cmd
@@ -0,0 +1,13 @@
+/* Text: Restore Postgres Database via pg_restore */
+             CMD        PROMPT('Restore Postgres DB-pg_restore')
+             PARM       KWD(DATABASE) TYPE(*CHAR) LEN(255) MIN(1) +
+                          CASE(*MIXED) PROMPT('Postgres database name')
+             PARM       KWD(INPUTFILE) TYPE(*CHAR) LEN(255) MIN(1) +
+                          CASE(*MIXED) PROMPT('From IFS tar input +
+                          file')
+             PARM       KWD(OPTIONS) TYPE(*CHAR) LEN(100) DFT(' ') +
+                          SPCVAL((*CLEARBEFORE '-c')) CASE(*MIXED) +
+                          PROMPT('Options')
+             PARM       KWD(PROMPT) TYPE(*CHAR) LEN(4) RSTD(*YES) +
+                          DFT(*NO) VALUES(*NO *YES) +
+                          PROMPT('Prompt for QSH command')
