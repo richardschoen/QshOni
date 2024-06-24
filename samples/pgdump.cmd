1f1c311730e7b87b234b3a3e3600fa42bd0bbd68
Richard Schoen <richard@richardschoen.net>
2024-06-24 11:45:45 -0500

    Update pgdump.cmd

--- samples/pgdump.cmd
+++ samples/pgdump.cmd
@@ -5,17 +5,29 @@
              CMD        PROMPT('Backup Postgres DB via pg_dump')
              PARM       KWD(DATABASE) TYPE(*CHAR) LEN(255) MIN(1) +
                           CASE(*MIXED) PROMPT('Postgres database name')
-             PARM       KWD(OUTPUTFILE) TYPE(*CHAR) LEN(255) MIN(1) +
+             PARM       KWD(OUTPUTFILE) TYPE(*CHAR) LEN(255) +
+                          DFT(*DBTEMPDATETIME) +
+                          SPCVAL((*DBTEMPDATETIME *DBTEMPDATETIME) +
+                          (*DBTEMPDATE *DBTEMPDATE)) MIN(0) +
                           CASE(*MIXED) PROMPT('To IFS output file')
              PARM       KWD(FORMAT) TYPE(*CHAR) LEN(10) DFT('t') +
                           CASE(*MIXED) PROMPT('Database dump output +
                           format')
              PARM       KWD(OPTIONS) TYPE(*CHAR) LEN(100) DFT('-p +
-                          5432 -U postgres') CASE(*MIXED) +
-                          PROMPT('Options')
+                          5432 -U postgres --verboase') +
+                          CASE(*MIXED) PROMPT('Options')
              PARM       KWD(PROMPT) TYPE(*CHAR) LEN(4) RSTD(*YES) +
                           DFT(*NO) VALUES(*NO *YES) CASE(*MIXED) +
                           PROMPT('Prompt for QSHBASH')
              PARM       KWD(REPLACE) TYPE(*CHAR) LEN(4) RSTD(*YES) +
                           DFT(*NO) VALUES(*NO *YES) CASE(*MIXED) +
                           PROMPT('Replace output file')
+             PARM       KWD(DSPSTDOUT) TYPE(*CHAR) LEN(4) RSTD(*YES) +
+                          DFT(*NO) VALUES(*NO *YES) PROMPT('Display +
+                          Standard Output Result')
+             PARM       KWD(LOGSTDOUT) TYPE(*CHAR) LEN(4) RSTD(*YES) +
+                          DFT(*NO) VALUES(*NO *YES) PROMPT('Log +
+                          standard output to job log')
+             PARM       KWD(PRTSTDOUT) TYPE(*CHAR) LEN(4) RSTD(*YES) +
+                          DFT(*NO) VALUES(*NO *YES) PROMPT('Print +
+                          Standard Output Result')
